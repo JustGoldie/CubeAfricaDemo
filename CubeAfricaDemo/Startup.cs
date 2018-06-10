fce75ed0da797ccf7c339ac02318b009bebe5a3b
JustGoldie <justin.goldie@gmail.com>
2018-06-10 17:08:17 +0200

    Add project files.

---
+++ CubeAfricaDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CubeAfricaDemo.Startup))]
+namespace CubeAfricaDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
